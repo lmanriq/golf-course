edee856914a878d15c2b49277733cb8db0bb175d
Lili Manrique <liliana.manrique1@gmail.com>
2019-12-26 12:03:20 -0700

    Pass tests

--- golf-course.js
+++ golf-course.js
@@ -4,6 +4,28 @@ class GolfCourse {
     this.difficulty = difficulty;
     this.openings = openings;
     this.features = features;
+    this.currentlyPlaying = [];
+  }
+
+  checkInGroup(group) {
+    if (group.length < this.openings) {
+      this.openings -= group.length;
+      for (var i = 0; i < group.length; i++) {
+        this.currentlyPlaying.unshift(group[i].name);
+      }
+    }
+  }
+
+  recommendTees(golfer) {
+    var difficulty;
+    if (golfer.handicap <= 10) {
+      difficulty = 'Difficult';
+    } else if (golfer.handicap >= 15) {
+      difficulty = 'Forward';
+    } else {
+      difficulty = 'Middle';
+    }
+    return `You should play from the ${difficulty} Tees.`;
   }
 }
 
